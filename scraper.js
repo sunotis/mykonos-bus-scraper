d29331d923f2b895d85f77616a187b4d8a506c1e
sito schwarzenberger <sitoschwarzenberger@sitos-Mac-mini.local>
2025-04-09 18:42:35 +0200

    Fix path import error

--- scraper.js
+++ scraper.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path'); // Add this line
 const app = express();
 
-// Allow requests from your web server
 app.use(cors({
     origin: 'https://mykonosbusmap.com'
 }));
